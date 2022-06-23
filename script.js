27cf13ac1859d7f49a7ba2216c8743cc7055dc17
OhJorgeReis <43137845+OhJorgeReis@users.noreply.github.com>
2022-06-23 04:18:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var video = document.getElementById("myVideo");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
